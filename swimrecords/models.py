3eaf3ad0f3965cfae804544949f224f78cac7d24
Won Yang <won@gatech.edu>
2021-06-24 09:23:44 -0500

    improves solution

--- swimrecords/models.py
+++ swimrecords/models.py
@@ -1,17 +1,12 @@
 from django.db import models
 from django.core.exceptions import ValidationError
 from django.utils import timezone
+from django.core.validators import MinValueValidator
 
 
 def validate_stroke(value):
     if not value in ['front crawl', 'butterfly', 'breast', 'back', 'freestyle']:
         raise ValidationError(f"{value} is not a valid stroke")
-
-
-def validate_distance(value):
-    if value < 50:
-        raise ValidationError(
-            "Ensure this value is greater than or equal to 50.")
 
 
 def validate_record_date(value):
@@ -25,9 +20,11 @@ class SwimRecord(models.Model):
     team_name = models.CharField(max_length=255)
     relay = models.BooleanField()
     stroke = models.CharField(max_length=255, validators=[validate_stroke])
-    distance = models.IntegerField(validators=[validate_distance])
-    record_date = models.DateTimeField(validators=[validate_record_date])
-    record_broken_date = models.DateTimeField()
+    distance = models.IntegerField(validators=[MinValueValidator(
+        50, message="Ensure this value is greater than or equal to 50.")])
+    record_date = models.DateTimeField(
+        default=timezone.now(), validators=[validate_record_date])
+    record_broken_date = models.DateTimeField(null=True)
 
     # need to override the clean methd for the last test case b/c we need to reference other fields besides record_broken_date
     def clean(self):
