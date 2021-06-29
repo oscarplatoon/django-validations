ff31b17be316915af282d278e3afd636933a32b1
Marc Stanley <marcson909@gmail.com>
2021-06-29 15:10:57 -0500

    completed challenge

--- .venv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/marcstanley/CodePlatoon/Week6/django-validations/.venv/bin/python3.9
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
