689529a3f447c60c526ddba390118f28db7dc145
vzbrunstad <vzbrunstad@msn.com>
2021-06-30 10:40:27 -0500

    Jonas P and Vince B Django-Validations

---
+++ .venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/vzbrunstad/code/oscar/challenges/week_6/week_6.2/django-validations/.venv/bin/python3.9
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
--- swimrecords/models.py
+++ swimrecords/models.py
@@ -1,12 +1,23 @@
+from datetime import datetime
+from django import test
+from django.core.exceptions import ValidationError
 from django.db import models
+from django.utils import timezone
+from django.core.validators import MaxValueValidator
+
+# Resolves test 5
+def validate_stroke(value):
+    valid_strokes = ['front crawl', 'butterfly', 'breast', 'back', 'freestyle']
+    if value not in valid_strokes:
+        raise ValidationError(f"{value} is not a valid stroke")
 
 class SwimRecord(models.Model):
-    pass # delete me when you start writing in validations
-    # first_name = models.CharField()
-    # last_name = models.CharField()
-    # team_name = models.CharField()
-    # relay = models.BooleanField()
-    # stroke = models.CharField()
-    # distance = models.IntegerField()
-    # record_date = models.DateTimeField()
-    # record_broken_date = models.DateTimeField()
+    first_name = models.CharField(max_length=200, null=False)
+    last_name = models.CharField(max_length=200, null=False)
+    team_name = models.CharField(max_length=200, null=False)
+    relay = models.BooleanField(null=False)
+    stroke = models.CharField(max_length=200, null=False, validators=[validate_stroke])
+    distance = models.IntegerField()
+    record_date = models.DateTimeField()
+    record_broken_date = models.DateTimeField()
+
